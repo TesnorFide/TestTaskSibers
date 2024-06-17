eafb6e56a86b7e9798cd30177ebf2ecaf922c6b1
Artem Shepetilo <105707009+TesnorFide@users.noreply.github.com>
2024-06-18 03:20:21 +0700

    Update bdconnect.php

--- bdconnect.php
+++ bdconnect.php
@@ -2,6 +2,6 @@
 
 $tbName = "userSibers"; // Name of table in database
 
-$link = DbConnect("localhost", "u2239489_test", "qwerty12345", "u2239489_test"); // Get link to connect database
+$link = DbConnect("localhost", USER_NAME, PASSWORD, DATABASE_NAME); // Get link to connect database
 
 ?>
